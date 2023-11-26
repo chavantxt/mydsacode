32de5e81b1ca3303565e6b66b2217a1f5f095c85
chavantxt <tejaswini.chavan2727@gmail.com>
2023-11-26 09:41:21 +0530

    Add existing file

---
+++ JavaBasic.java
@@ -0,0 +1,22 @@
+package mydsacode;
+
+import java.util.ArrayList;
+
+public class JavaBasic {
+
+    public static void main(String args[]){
+        
+        ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    System.out.println(cars);	    
+	    cars.get(0);
+	    cars.set(0, "Opel");
+	    cars.remove(0);
+	    cars.clear();
+	    cars.size();
+    }
+    
+}
