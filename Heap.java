c41e5692e81a568d3c5cba527aad926b8fa5173b
chavantxt <tejaswini.chavan2727@gmail.com>
2024-07-30 17:02:59 +0530

    Heap

--- Heap.java
+++ Heap.java
@@ -3,6 +3,22 @@ import java.util.*;
 public class Heap {
 
     public static int KthSmallestNumber(){
+        int arr[]={2,55,44,0,8};
+        int k=3;
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+        for(int i=0;i<arr.length;i++){
+            pq.add(arr[i]); 
+            if(pq.size()>k)
+            {
+                    pq.poll();
+            }
+              
+        }
+        return pq.peek();
+    }
+
+    public static int KthLargestNumber(){
+
         int arr[]={2,3,44,0,8,4};
         int k=3;
         PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
@@ -10,7 +26,7 @@ public class Heap {
                
             if(pq.size()>k)
             {
-                if(pq.peek()>arr[i])
+                if(pq.peek()<arr[i])
                     pq.poll();
             }
             else
@@ -18,9 +34,10 @@ public class Heap {
         }
 
         return pq.peek();
-    }
+    } 
     public static void main(String args[]){
-        System.out.println(KthSmallestNumber());
-
+       System.out.println(KthSmallestNumber());
+       // System.out.println(KthLargestNumber());
+       System.out.println("Hello Wold!");
     }
 }
